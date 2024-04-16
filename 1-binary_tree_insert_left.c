fb817eaa74662befc6075f40edc0249a26629f00
dacods <135403583+dacods@users.noreply.github.com>
2024-04-16 10:11:41 -0500

    Changed 1-binary_tree_insert_left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
         return(NULL);
     }
     
-    binary_tree_t *new_left_node = (binary_tree_t*)malloc(sizeof(binary_tree_t));
+    binary_tree_t *new_left_node = malloc(sizeof(binary_tree_t));
     if (new_left_node == NULL)
     {
         return (NULL);
