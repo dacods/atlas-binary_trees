fba8ff057a27b9726be641ac39477be4f7549ba1
dacods <135403583+dacods@users.noreply.github.com>
2024-04-18 13:57:54 -0500

    Added 2-binary_tree_insert_right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,31 +5,31 @@
  * @parent: Pointer to the node to insert the right-child in
  * @value: Value to store in the new node
  * Return: new node or NULL if failure
-*/
+ */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-    new_node = binary_tree_node(parent, value);
+	new_node = binary_tree_node(parent, value);
 
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    if (parent->right != NULL)
-    {
-        new_node->right = parent->right;
-        new_node->right->parent = new_node;
-    }
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
+	}
 
-    parent->right = new_node;
+	parent->right = new_node;
 
-    return (new_node);
+	return (new_node);
 }
