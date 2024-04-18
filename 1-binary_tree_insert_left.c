23089616895fc5857f2893520003a8a80bf380b1
dacods <135403583+dacods@users.noreply.github.com>
2024-04-18 13:44:38 -0500

    Fixed 1-binary_tree_insert_left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,31 +5,29 @@
  * @parent: Pointer to the node to insert
  * @value: value to store in the new node
  * Return: Pointer to created node or NULL on failure
-*/
+ */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
-    
-    if (parent == NULL)
-    {
-        return(NULL);
-    }
+	binary_tree_t *new_node;
 
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-    if (parent->left != NULL)
-    {
-        new_node->left = parent->left;
-        new_node->left->parent = new_node;
-    }
-    parent->left = new_node;
-    
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    return (new_node);
-    
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+	}
+	parent->left = new_node;
+
+	return (new_node);
 }
