cb752754adcfb5414c258189fb93c2202b07825f
dacods <135403583+dacods@users.noreply.github.com>
2024-04-19 14:05:06 -0500

    Added 10-binary_tree_depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Function that measures the depth of a node
+ * @tree: Pointer to the node to measure
+ * Return: The depth of the node
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	while (tree->parent != NULL)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
+}
