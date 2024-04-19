546c53a1026ef100711754f8a412e473bd33a67d
dacods <135403583+dacods@users.noreply.github.com>
2024-04-19 13:47:40 -0500

    Fixed 9-binary_tree_height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,6 +15,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+
 	height_l = binary_tree_height(tree->left);
 
 	height_r = binary_tree_height(tree->right);
