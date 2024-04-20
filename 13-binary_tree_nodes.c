24364708a42b2320c72513e1eda33b815feff543
dacods <135403583+dacods@users.noreply.github.com>
2024-04-19 21:45:45 -0500

    Fixed 13-binary_tree_nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -4,19 +4,19 @@
  * binary_tree_nodes - Function that counts nodes with at least 1 child
  * @tree: Pointer to the root node
  * Return: Counted nodes or 0 if tree is NULL
-*/
+ */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (!tree)
-    {
-        return (0);
-    }
+	if (!tree)
+	{
+		return (0);
+	}
 
-    if (tree->left != NULL || tree->right != NULL)
-    {
-        return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
-    }
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	}
 
-    return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
