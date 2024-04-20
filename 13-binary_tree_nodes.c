096e34b7d8bd6b8b9f8ff835c510382d0247a47b
dacods <135403583+dacods@users.noreply.github.com>
2024-04-19 21:44:31 -0500

    Added 13-binary_tree_nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Function that counts nodes with at least 1 child
+ * @tree: Pointer to the root node
+ * Return: Counted nodes or 0 if tree is NULL
+*/
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    if (!tree)
+    {
+        return (0);
+    }
+
+    if (tree->left != NULL || tree->right != NULL)
+    {
+        return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+    }
+
+    return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
